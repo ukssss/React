fe53b2a2508c482aaba4c5e84124337b447d2c1f
ukssss <1872003115su@gmail.com>
2023-02-21 23:36:05 +0900

    01-react-introduction | JSX 실습
    - Export JSX
    - CSS, Inline CSS
    - 변수 적용 {}

--- 01.udemy-react-example/react-introduction/reactexam1/src/App.js
+++ 01.udemy-react-example/react-introduction/reactexam1/src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
+import React from 'react';
+// import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
 
 function App() {
-  let name = 'ukss';
+  const style = {
+    App: {
+      backgroundColor: 'black',
+    },
+    h2: {
+      color: 'red',
+    },
+    bold_text: {
+      color: 'green',
+    },
+  };
+
+  const number = 5;
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+      <h2 style={style.h2}>안녕 리액트</h2>
+      <b style={style.bold_text} id='bold_text'>
+        {number}는 : {number % 2 === 0 ? '짝수' : '홀수'}
+      </b>
+      <MyFooter />
     </div>
   );
 }
