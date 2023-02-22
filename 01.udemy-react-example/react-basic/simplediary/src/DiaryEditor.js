a2e0d77ee93d259d20a7decb517cc4e0517b888a
ukssss <1872003115su@gmail.com>
2023-02-22 22:53:24 +0900

    01-react-basic | refactor, useState를 활용한 handleChangeState 함수 생성

--- 01.udemy-react-example/react-basic/simplediary/src/DiaryEditor.js
+++ 01.udemy-react-example/react-basic/simplediary/src/DiaryEditor.js
@@ -7,63 +7,30 @@ const DiaryEditor = () => {
     emotion: 1,
   });
 
-  // const handleChangeState = (e) => {
-  //   setState({
-  //     ...state,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // };
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = () => {
     console.log(state);
+    alert('저장 완료 ☺️');
   };
 
   return (
     <div className='DiaryEditor'>
       <h2>오늘의 일기</h2>
       <div>
-        <input
-          value={state.author}
-          onChange={(e) => {
-            setState({
-              author: e.target.value,
-              content: state.content,
-              emotion: state.emotion,
-            });
-          }}
-          name='author'
-          placeholder='작성자'
-          type='text'
-        />
+        <input value={state.author} onChange={handleChangeState} name='author' placeholder='작성자' type='text' />
       </div>
       <div>
-        <textarea
-          value={state.content}
-          onChange={(e) => {
-            setState({
-              author: state.author,
-              content: e.target.value,
-              emotion: state.emotion,
-            });
-          }}
-          name='content'
-          placeholder='내용'
-          type='text'
-        />
+        <textarea value={state.content} onChange={handleChangeState} name='content' placeholder='내용' type='text' />
       </div>
       <div>
         <span>오늘의 감정점수 : </span>
-        <select
-          value={state.emotion}
-          onChange={(e) => {
-            setState({
-              author: state.author,
-              content: state.content,
-              emotion: e.target.value,
-            });
-          }}
-          name='emotion'
-        >
+        <select value={state.emotion} onChange={handleChangeState} name='emotion'>
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
