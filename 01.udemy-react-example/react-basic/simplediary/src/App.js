2a0f3a709f7caee42239b6188c3f6b5e2ffc7b32
ukssss <1872003115su@gmail.com>
2023-02-24 00:00:47 +0900

    01-react-basic | 데이터 추가 실습 #29
    - onCreate 함수를 제작해 데이터를 추가
    - setData 함수를 제작해 데이터 추가 후 입력창을 초기화

--- 01.udemy-react-example/react-basic/simplediary/src/App.js
+++ 01.udemy-react-example/react-basic/simplediary/src/App.js
@@ -1,36 +1,54 @@
+import React, { useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id: 1,
-    author: 'ukss',
-    content: '안녕~',
-    emotion: 3,
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: '홍성욱',
-    content: '안녕~~',
-    emotion: 1,
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: '욱쓰',
-    content: '안녕~~~',
-    emotion: 4,
-    createdDate: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: 'ukss',
+//     content: '안녕~',
+//     emotion: 3,
+//     createdDate: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: '홍성욱',
+//     content: '안녕~~',
+//     emotion: 1,
+//     createdDate: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: '욱쓰',
+//     content: '안녕~~~',
+//     emotion: 4,
+//     createdDate: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const createdDate = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      createdDate,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className='App'>
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
