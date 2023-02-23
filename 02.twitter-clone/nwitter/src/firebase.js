267d6b392d8df5cda3607fed55638f572b5344ad
ukssss <1872003115su@gmail.com>
2023-02-23 15:16:15 +0900

    02-twitter | Firebase 설치 및 설정 #22

---
+++ 02.twitter-clone/nwitter/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-QG51MrRjo9VCI1jc7zbr8sVm5NTSijw',
+  authDomain: 'nwitter-4ad40.firebaseapp.com',
+  projectId: 'nwitter-4ad40',
+  storageBucket: 'nwitter-4ad40.appspot.com',
+  messagingSenderId: '448884931129',
+  appId: '1:448884931129:web:f45e383b42496c20476be8',
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
