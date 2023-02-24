e82fdc6c5dbbe674a3495b5051c7f207d0e0e78d
ukssss <1872003115su@gmail.com>
2023-02-25 01:30:08 +0900

    02-twitter | learn, Log Out 구현 #48
    - useNavigate 사용
    - v6 이후로는 useHistory, redirect 를 사용할 수 없기에 useNavigate 를 사용

--- 02.twitter-clone/nwitter/src/components/Router.js
+++ 02.twitter-clone/nwitter/src/components/Router.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Profile from '../routes/Profile';
+import Navigation from './Navigation';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path='/' element={<Home />}></Route>
+            <Route path='/profile' element={<Profile />}></Route>
           </>
         ) : (
-          <Route path='/' element={<Auth />}></Route>
+          <>
+            <Route path='/' element={<Auth />}></Route>
+          </>
         )}
       </Routes>
     </Router>
