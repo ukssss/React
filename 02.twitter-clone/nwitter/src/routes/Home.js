62cc6d3aa157de0d114e980718a8e3497af0e83e
ukssss <1872003115su@gmail.com>
2023-02-25 16:50:33 +0900

    02-twitter | learn, Database 셋업 #50
    - firebase/database

--- 02.twitter-clone/nwitter/src/routes/Home.js
+++ 02.twitter-clone/nwitter/src/routes/Home.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type='text' placeholder="what's on your mind?" maxLength={120}></input>
+        <input type='submit' value='Nweet' />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
