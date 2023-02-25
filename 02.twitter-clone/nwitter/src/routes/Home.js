80bf4835a4509b8bd8be855ac564ed1724190ff1
ukssss <1872003115su@gmail.com>
2023-02-25 17:20:13 +0900

    02-twitter | learn, Nweeting to Firestore #51
    - 입력값을 Firestore 로 보내는 작업 구현

--- 02.twitter-clone/nwitter/src/routes/Home.js
+++ 02.twitter-clone/nwitter/src/routes/Home.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import { dbService } from 'fbase';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 
 const Home = () => {
   const [nweet, setNweet] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+
+    console.log(`서브밋 하는 느윗: ${nweet}`);
+
+    await addDoc(collection(dbService, 'nweet'), {
+      nweet,
+      createdAt: serverTimestamp(),
+    });
+
+    setNweet('');
   };
 
   const onChange = (e) => {
