343a98816bdfa577b84be0d31684b487bae85e1c
ukssss <1872003115su@gmail.com>
2023-02-26 23:44:02 +0900

    02-twitter | learn, Delete and Update #54
    - 작성자 본인이 아닐 경우 버튼이 출력되지 않도록 제작

--- 02.twitter-clone/nwitter/src/routes/Home.js
+++ 02.twitter-clone/nwitter/src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
 import { collection, addDoc, onSnapshot, query, orderBy, serverTimestamp } from 'firebase/firestore';
+import Nweet from 'components/Nweet';
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
@@ -44,9 +45,7 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.text}</h4>
-          </div>
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
         ))}
       </div>
     </>
