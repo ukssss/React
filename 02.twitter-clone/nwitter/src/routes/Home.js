635c522d48cfa4c3185e52dace9e1603728458a6
ukssss <1872003115su@gmail.com>
2023-02-26 20:29:32 +0900

    02-twitter | learn, Firestore 에 저장되어 있는 Nweets 출력 #52
    - Create, Read 구현

--- 02.twitter-clone/nwitter/src/routes/Home.js
+++ 02.twitter-clone/nwitter/src/routes/Home.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, serverTimestamp, getDocs, query } from 'firebase/firestore';
 
 const Home = () => {
   const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const querySnapshot = await getDocs(query(collection(dbService, 'nweets')));
+
+    querySnapshot.forEach((doc) => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
     console.log(`서브밋 하는 느윗: ${nweet}`);
 
-    await addDoc(collection(dbService, 'nweet'), {
+    await addDoc(collection(dbService, 'nweets'), {
       nweet,
       createdAt: serverTimestamp(),
     });
@@ -31,6 +48,13 @@ const Home = () => {
         <input value={nweet} onChange={onChange} type='text' placeholder="what's on your mind?" maxLength={120}></input>
         <input type='submit' value='Nweet' />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
