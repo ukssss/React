8029e52f11564ec85e16a19ce21d6cac535a835f
ukssss <1872003115su@gmail.com>
2023-02-27 13:39:44 +0900

    02-twitter | learn, Storage 에 Upload 기능 구현 #60
    - uuid 설치
    - getStorage()

--- 02.twitter-clone/nwitter/src/routes/Home.js
+++ 02.twitter-clone/nwitter/src/routes/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
 import { collection, addDoc, onSnapshot, query, orderBy, serverTimestamp } from 'firebase/firestore';
+import { ref, uploadString } from 'firebase/storage';
 import Nweet from 'components/Nweet';
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('');
@@ -22,13 +24,18 @@ const Home = ({ userObj }) => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await addDoc(collection(dbService, 'nweets'), {
-      text: nweet,
-      createdAt: serverTimestamp(),
-      creatorId: userObj.uid,
-    });
+    // await addDoc(collection(dbService, 'nweets'), {
+    //   text: nweet,
+    //   createdAt: serverTimestamp(),
+    //   creatorId: userObj.uid,
+    // });
 
-    setNweet('');
+    // setNweet('');
+
+    const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+    const resp = await uploadString(attachmentRef, attachment, 'data_url');
+
+    console.log(resp);
   };
 
   const onChange = (e) => {
