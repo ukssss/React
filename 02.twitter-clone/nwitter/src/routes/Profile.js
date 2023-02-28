11ce5fbccd5af45a491387a7ae2860f5c8d1bd67
ukssss <1872003115su@gmail.com>
2023-02-28 10:43:33 +0900

    02-twitter | learn, 트윗 필터링 작업 #64

--- 02.twitter-clone/nwitter/src/routes/Profile.js
+++ 02.twitter-clone/nwitter/src/routes/Profile.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { authService } from 'fbase';
+import React, { useEffect } from 'react';
+import { authService, dbService } from 'fbase';
+import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
     navigate('/');
   };
+
+  const getMyNweets = async () => {
+    const q = query(collection(dbService, 'nweets'), where('creatorId', '==', userObj.uid), orderBy('createdAt', 'desc'));
+    onSnapshot(q, (querySnapshot) => {
+      const nweetArr = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log(nweetArr);
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
