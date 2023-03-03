e69e198379a5d6fa032e0471ca8419631071fbb2
ukssss <1872003115su@gmail.com>
2023-03-03 22:33:51 +0900

    01-react-basic | learn, React Lifecyle 제어 - useEffect Mount, Unmount #32

--- 01.udemy-react-example/react-basic/simplediary/src/Lifecycle.js
+++ 01.udemy-react-example/react-basic/simplediary/src/Lifecycle.js
@@ -1,38 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-
+const UnmountTest = () => {
   useEffect(() => {
-    console.log('Mount!');
+    console.log('Mount');
+
+    return () => {
+      // Unmount 시점에 실행
+      console.log('Unmount');
+    };
   }, []);
 
-  useEffect(() => {
-    console.log('Update');
-  });
+  return <div>Unmount Testing Component</div>;
+};
 
-  useEffect(() => {
-    console.log(`count is update : ${count}`);
-    if (count > 5) {
-      alert('count가 5를 넘었습니다. 따라서 1로 초기화합니다');
-      setCount(1);
-    }
-  }, [count]);
-
-  useEffect(() => {
-    console.log(`text is update : ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
