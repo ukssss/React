5fa5fa41019d538fa083aa24b000d828c9fb22de
ukssss <1872003115su@gmail.com>
2023-03-05 22:34:21 +0900

    01-react-basic | learn, 최적화 기법 - useMemo #35

--- 01.udemy-react-example/react-basic/simplediary/src/App.js
+++ 01.udemy-react-example/react-basic/simplediary/src/App.js
@@ -1,9 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-
-// https://jsonplaceholder.typicode.com/comments
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -53,9 +51,25 @@ const App = () => {
     setData(data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className='App'>
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
