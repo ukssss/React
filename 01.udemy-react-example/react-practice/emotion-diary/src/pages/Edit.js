8ce832aae7230a51ebd01f97f78e60ceded2781c
ukssss <1872003115su@gmail.com>
2023-03-07 16:51:52 +0900

    01-react-practice | learn, 페이징 라우팅 2 - React Router 응용 #76
    - useParams
    - useNavigate

--- 01.udemy-react-example/react-practice/emotion-diary/src/pages/Edit.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/pages/Edit.js
@@ -1,8 +1,33 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id');
+  const mode = searchParams.get('mode');
+  console.log('id : ', id);
+  console.log('mode : ', mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: 'ukss' })}>QS 바꾸기</button>
+      <button
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
