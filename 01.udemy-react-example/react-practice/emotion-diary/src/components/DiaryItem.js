9a1b90c48bff29972042fe74d880488c529720b1
ukssss <1872003115su@gmail.com>
2023-03-08 17:14:41 +0900

    01-react-practice | add, 페이지 구현 - 홈(/) - 날짜, 내용 출력 #79

--- 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryItem.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryItem.js
@@ -2,12 +2,17 @@ const DiaryItem = ({ id, emotion, content, date }) => {
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || '';
 
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
   return (
     <div className='DiaryItem'>
       <div className={['emotion_img_wrapper', `emotion_img_wrapper_${emotion}`].join(' ')}>
         <img src={env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt='감정상태' />
       </div>
-      <div></div>
+      <div className='info_wrapper'>
+        <div className='diary_date'>{strDate}</div>
+        <div className='diary_content_preview'>{content.slice(0, 25)}</div>
+      </div>
       <div></div>
     </div>
   );
