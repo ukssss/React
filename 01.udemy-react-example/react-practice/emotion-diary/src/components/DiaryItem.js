d49de232c774949c57ebcae342105999f44462fd
ukssss <1872003115su@gmail.com>
2023-03-08 17:24:55 +0900

    01-react-practice | add, 페이지 구현 - 홈(/) - 수정하기 버튼 추가 및 navigate 연결 #79

--- 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryItem.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryItem.js
@@ -1,19 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+import MyButton from './MyButton';
+
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || '';
 
   const strDate = new Date(parseInt(date)).toLocaleDateString();
 
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <div className='DiaryItem'>
-      <div className={['emotion_img_wrapper', `emotion_img_wrapper_${emotion}`].join(' ')}>
+      <div onClick={goDetail} className={['emotion_img_wrapper', `emotion_img_wrapper_${emotion}`].join(' ')}>
         <img src={env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt='감정상태' />
       </div>
-      <div className='info_wrapper'>
+      <div onClick={goDetail} className='info_wrapper'>
         <div className='diary_date'>{strDate}</div>
         <div className='diary_content_preview'>{content.slice(0, 25)}</div>
       </div>
-      <div></div>
+      <div onClick={goEdit} className='btn_wrapper'>
+        <MyButton text={'수정하기'} />
+      </div>
     </div>
   );
 };
