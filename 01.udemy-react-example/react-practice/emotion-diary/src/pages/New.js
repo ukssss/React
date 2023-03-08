a3582f964f02e0a5de381542e90deafb5473843b
ukssss <1872003115su@gmail.com>
2023-03-08 20:41:13 +0900

    01-react-practice | add, 페이지 구현 -일기 쓰기(/new) - 오늘은 언제인가요 완성 #80

--- 01.udemy-react-example/react-practice/emotion-diary/src/pages/New.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/pages/New.js
@@ -1,14 +1,9 @@
-import { useNavigate } from 'react-router-dom';
-
-import MyHeader from '../components/MyHeader';
-import MyButton from '../components/MyButton';
+import DiaryEditor from '../components/DiaryEditor';
 
 const New = () => {
-  const navigate = useNavigate();
-
   return (
     <div>
-      <MyHeader headText={'새 일기쓰기'} leftChild={<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />} />
+      <DiaryEditor />
     </div>
   );
 };
