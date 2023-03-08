594487cc0a34a38234c1bc750965c7b89d751418
ukssss <1872003115su@gmail.com>
2023-03-08 10:46:07 +0900

    01-react-practice | setting, 프로젝트 기초 공사 1 - 공통 컴포넌트 세팅 #77
    - 모든 페이지에 공통으로 사용되는 버튼, 헤더 컴포넌트 세팅

--- 01.udemy-react-example/react-practice/emotion-diary/src/App.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/App.js
@@ -6,6 +6,10 @@ import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
+// Components
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 function App() {
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || '';
@@ -13,13 +17,17 @@ function App() {
   return (
     <BrowserRouter>
       <div className='App'>
+        <MyHeader
+          headText={'App'}
+          leftChild={<MyButton text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭')} />}
+          rightChild={<MyButton text={'오른쪽 버튼'} onClick={() => alert('오른쪽 클릭')} />}
+        />
         <h2>App.js</h2>
-        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
 
+        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} type={'positive'} />
+        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} type={'negative'} />
+        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} type={'qwrqqwrqwr'} />
+        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
