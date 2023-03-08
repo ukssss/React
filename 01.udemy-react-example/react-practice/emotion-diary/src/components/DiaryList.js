b4d6857b27a9f276adc9aca92e749306e86f1c51
ukssss <1872003115su@gmail.com>
2023-03-08 16:34:26 +0900

    01-react-practice | add, 페이지 구현 - 홈(/) - 새 일기쓰기 버튼 추가 #79
    - useNavigate 를 사용하여 /new 로 이동할 수 있도록 제작

--- 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryList.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryList.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MyButton from './MyButton';
 
 const sortOptionList = [
   { value: 'latest', name: '최신순' },
@@ -13,7 +15,7 @@ const filterOptionList = [
 
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <select className='ControlMenu' value={value} onChange={(e) => onChange(e.target.value)}>
       {optionList.map((it, idx) => (
         <option key={idx} value={it.value}>
           {it.name}
@@ -24,6 +26,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 };
 
 const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
   const [sortType, setSortType] = useState('latest');
   const [filter, setFilter] = useState('all');
 
@@ -52,9 +55,17 @@ const DiaryList = ({ diaryList }) => {
   };
 
   return (
-    <div>
-      <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
-      <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList} />
+    <div className='DiaryList'>
+      <div className='menu_wrapper'>
+        <div className='left_col'>
+          <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
+          <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList} />
+        </div>
+        <div className='right_col'>
+          <MyButton type={'positive'} text={'새 일기쓰기'} onClick={() => navigate('/new')} />
+        </div>
+      </div>
+
       {getProcessedDiaryList().map((it) => (
         <div key={it.id}>
           {it.content} {it.emotion}
