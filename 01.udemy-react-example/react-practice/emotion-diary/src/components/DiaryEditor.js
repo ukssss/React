6e640a40f160d72dcab44faee227001ef91559f4
ukssss <1872003115su@gmail.com>
2023-04-03 13:15:16 +0900

    01-react-practice | add, 페이지 구현 - 일기 쓰기(/new) - 오늘의 일기 완성 #80

--- 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryEditor.js
+++ 01.udemy-react-example/react-practice/emotion-diary/src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import MyHeader from './MyHeader';
@@ -41,6 +41,8 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
+  const contentRef = useRef();
+  const [content, setContent] = useState('');
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
@@ -71,6 +73,17 @@ const DiaryEditor = () => {
                 isSelected={it.emotion_id === emotion}
               />
             ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className='input_box text_wrapper'>
+            <textarea
+              placeholder='오늘은 어땠나요?'
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
           </div>
         </section>
       </div>
