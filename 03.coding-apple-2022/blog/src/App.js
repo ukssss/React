ce74622ce7e5c3ce0ef8d32cc59865985e9076e7
ukssss <1872003115su@gmail.com>
2023-04-08 22:36:07 +0900

    03-coding-apple-2022 | state 실습
    - 자주 변경될 것 같은 html 부분은 'state'로 만들어 놓기

--- 03.coding-apple-2022/blog/src/App.js
+++ 03.coding-apple-2022/blog/src/App.js
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let post = '신림동 24시 서울밥집';
+  let [first, setFirst] = useState('신림 24시 서울밥집');
+  let [second, setSecond] = useState('낙성대 기절초풍왕순대');
+  let [third, setThird] = useState('서울대입구 산골');
 
   return (
     <div className='App'>
       <div className='black-nav'>
-        <h4 style={{ color: 'red', fontSize: '16px' }}>블로그</h4>
+        <h4>ukss's blog</h4>
       </div>
-      <h4>{post}</h4>
+      <div className='list'>
+        <h4>{first}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{second}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{third}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// 정리
+// 자주 변경될 것 같은 html 부분은 'state'로 만들어 놓기!
