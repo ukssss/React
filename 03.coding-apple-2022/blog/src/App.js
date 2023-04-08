134c12805244aee5cc0f76773f7abe6877c38cd9
ukssss <1872003115su@gmail.com>
2023-04-08 19:09:56 +0900

    03-coding-apple-2022 | JSX 기본 문법 3가지
    - class 넣을 땐 className
    - 변수 꽂을 땐 {변수명}
    - style 넣을 땐 style={{이름:'값'}}

--- 03.coding-apple-2022/blog/src/App.js
+++ 03.coding-apple-2022/blog/src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let post = '신림동 24시 서울밥집';
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>uksssss</p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4 style={{ color: 'red', fontSize: '16px' }}>블로그</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
