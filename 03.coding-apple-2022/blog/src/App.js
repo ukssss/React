3bcd63f0a937e52e3b87084da4731c9b0b82f183
ukssss <1872003115su@gmail.com>
2023-04-10 22:32:21 +0900

    03-coding-apple-2022 | 버튼 제작 & 과제
    - onClick 사용법, onClick={} 안엔 함수를 넣어야 한다.
    - state 변경하는 법, 항상 state 변경함수 쓸 것 (새로운 state)

--- 03.coding-apple-2022/blog/src/App.js
+++ 03.coding-apple-2022/blog/src/App.js
@@ -1,26 +1,41 @@
+/* eslint-disable */
+
 import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [first, setFirst] = useState('신림 24시 서울밥집');
-  let [second, setSecond] = useState('낙성대 기절초풍왕순대');
-  let [third, setThird] = useState('서울대입구 산골');
+  let [matzip, setMatzip] = useState(['신림 24시 서울밥집', '낙성대 기절초풍왕순대', '서울대입구 산골']);
+  let [good, setGood] = useState(0);
+
+  function handleGood() {
+    setGood(good++);
+  }
+
+  function handleChange() {
+    setMatzip(['압구정로데오 웍셔너리', '낙성대 기절초풍왕순대', '서울대입구 산골']);
+  }
 
   return (
     <div className='App'>
       <div className='black-nav'>
         <h4>ukss's blog</h4>
       </div>
+
+      <button onClick={handleChange}>🙋🏼‍♀️</button>
+
       <div className='list'>
-        <h4>{first}</h4>
+        <h4>
+          {matzip[0]} <span onClick={handleGood}>👍🏻</span> {good}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className='list'>
+        <h4>{matzip[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{second}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{third}</h4>
+        <h4>{matzip[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
@@ -30,4 +45,5 @@ function App() {
 export default App;
 
 // 정리
-// 자주 변경될 것 같은 html 부분은 'state'로 만들어 놓기!
+// - onClick 사용법, onClick={} 안엔 함수를 넣어야 한다.
+// - state 변경하는 법, 항상 state 변경함수 쓸 것 (새로운 state)
