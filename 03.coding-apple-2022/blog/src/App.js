6a0839efb0093db588a9c53281d47738e9e02d97
ukssss <1872003115su@gmail.com>
2023-04-11 20:26:02 +0900

    03-coding-apple-2022 | 동적 UI 제작 (모달창 생성)

--- 03.coding-apple-2022/blog/src/App.js
+++ 03.coding-apple-2022/blog/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   let [matzip, setMatzip] = useState(['신림 24시 서울밥집', '낙성대 기절초풍왕순대', '서울대입구 산골']);
   let [good, setGood] = useState(0);
+  let [modal, setModal] = useState(false);
 
   function handleGood() {
     setGood(good++);
@@ -43,45 +44,46 @@ function App() {
       <button onClick={handleGanada}>가나다순 정렬</button>
 
       <div className='list'>
-        <h4>
+        <h4
+          onClick={() => {
+            modal ? setModal(false) : setModal(true);
+          }}
+        >
           {matzip[0]} <span onClick={handleGood}>👍🏻</span> {good}
         </h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className='list'>
-        <h4>{matzip[1]}</h4>
+        <h4
+          onClick={() => {
+            modal ? setModal(false) : setModal(true);
+          }}
+        >
+          {matzip[1]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className='list'>
-        <h4>{matzip[2]}</h4>
+        <h4
+          onClick={() => {
+            modal ? setModal(false) : setModal(true);
+          }}
+        >
+          {matzip[2]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
 
-      <Modal />
+      {modal ? <Modal /> : ''}
     </div>
   );
 }
 
 export default App;
 
-// 컴포넌트 만드는 법
-// 1. function 만들고
-// 2. return() 안에 html 담기
-// 3. <함수명></함수명> 쓰기
-
-// 의미없는 <div> 대신 <></> 사용가능 (fragment)
-// <함수명></함수명>, <함수명/> 둘 다 가능
-
-// 어떤걸 컴포넌트로 만들면 좋은가
-// 1. 반복적인 html 축약할 때
-// 2. 큰 페이지들
-// 3. 자주 변경되는 것들
-
-// 컴포넌트의 단점
-// state 가져다 쓸 경우 문제가 발생
-// (A 함수에 있던 변수는 B 함수에서 맘대로 가져다 쓸 수 없음)
-
-// 컴포넌트 만드는 문법 2
-// const Modal = () => {}
+// 동적인 UI 만드는 step
+// 1. html css 로 미리 디자인 완성
+// 2. UI의 현재 상태를 state로 저장
+// 3. state에 따라 UI가 어떻게 보일지 저장 (조건문 등으로)
