88b804cb8db067eff2a011248d9884c1f2646e37
ukssss <1872003115su@gmail.com>
2023-04-11 17:13:50 +0900

    03-coding-apple-2022 | 모달창 제작 (컴포넌트 실습)

--- 03.coding-apple-2022/blog/src/App.js
+++ 03.coding-apple-2022/blog/src/App.js
@@ -23,6 +23,16 @@ function App() {
     setMatzip(copy);
   }
 
+  function Modal() {
+    return (
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    );
+  }
+
   return (
     <div className='App'>
       <div className='black-nav'>
@@ -43,15 +53,35 @@ function App() {
         <h4>{matzip[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
       <div className='list'>
         <h4>{matzip[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal />
     </div>
   );
 }
 
 export default App;
 
-// 정리
-// - state가 array/object면 독립적 카피본을 만들어서 수정해야 함
+// 컴포넌트 만드는 법
+// 1. function 만들고
+// 2. return() 안에 html 담기
+// 3. <함수명></함수명> 쓰기
+
+// 의미없는 <div> 대신 <></> 사용가능 (fragment)
+// <함수명></함수명>, <함수명/> 둘 다 가능
+
+// 어떤걸 컴포넌트로 만들면 좋은가
+// 1. 반복적인 html 축약할 때
+// 2. 큰 페이지들
+// 3. 자주 변경되는 것들
+
+// 컴포넌트의 단점
+// state 가져다 쓸 경우 문제가 발생
+// (A 함수에 있던 변수는 B 함수에서 맘대로 가져다 쓸 수 없음)
+
+// 컴포넌트 만드는 문법 2
+// const Modal = () => {}
