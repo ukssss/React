7bba87afab3b4906bd16759d64df56cafa0ffe7e
ukssss <1872003115su@gmail.com>
2023-04-24 18:10:45 +0900

    03-coding-apple-2022 | Ajax 통신 1, Json-server 생성

--- 03.coding-apple-2022/shop/src/pages/Home.js
+++ 03.coding-apple-2022/shop/src/pages/Home.js
@@ -1,17 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import { useState } from 'react';
 
 function Home(props) {
+  let [shoes, setShoes] = useState('');
+
   function Product(props) {
     return (
       <>
-        {' '}
         <Col>
           <img src={props.img} alt='shoes' width='80%' />
           <h4>{props.title}</h4>
           <p>{props.content}</p>
-          <p>{props.price}</p>
+          <p>{props.price.toLocaleString()} 원</p>
         </Col>
-        ;
       </>
     );
   }
@@ -28,6 +30,33 @@ function Home(props) {
           })}
         </Row>
       </Container>
+      <button
+        onClick={() => {
+          axios
+            .get('http://localhost:4000/shoes')
+            .then((result) => {
+              setShoes(result.data);
+            })
+            .catch(() => {
+              console.log('실패했다 ㅋㅋ');
+            });
+        }}
+      >
+        버튼
+      </button>
+      {shoes ? (
+        <Container>
+          <Row>
+            {Array.from(shoes).map((item) => {
+              return (
+                <Product key={item.id} img={item.img} title={item.title} content={item.content} price={item.price} />
+              );
+            })}
+          </Row>
+        </Container>
+      ) : (
+        ''
+      )}
     </>
   );
 }
