26b5a1da2913556ffbe841bcb67c55b05282a789
ukssss <1872003115su@gmail.com>
2023-04-25 15:42:39 +0900

    03-coding-apple-2022 | 탭 UI 생성

--- 03.coding-apple-2022/shop/src/pages/Detail.js
+++ 03.coding-apple-2022/shop/src/pages/Detail.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import '../Detail.module.css';
+import { Nav } from 'react-bootstrap';
 
 function Detail(props) {
   let { id } = useParams();
   let product = props.shoes.find((item) => item.id === parseInt(id));
   let [visible, setVisible] = useState(true);
   let [num, setNum] = useState('');
+  let [tab, setTab] = useState(0);
 
   function popup() {
     setTimeout(() => {
@@ -47,6 +49,60 @@ function Detail(props) {
               <button className='btn btn-danger'>주문하기</button>
             </div>
           </div>
+          <Nav variant='tabs' defaultActiveKey='link0'>
+            <Nav.Item>
+              <Nav.Link
+                onClick={() => {
+                  setTab(0);
+                }}
+                eventKey='link0'
+              >
+                체결 거래
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                onClick={() => {
+                  setTab(1);
+                }}
+                eventKey='link1'
+              >
+                판매 입찰
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                onClick={() => {
+                  setTab(2);
+                }}
+                eventKey='link2'
+              >
+                구매 입찰
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          {
+            [
+              <div>
+                Aute nulla anim non proident. Sit officia officia proident nostrud consequat quis est culpa cupidatat
+                laboris duis eiusmod sunt veniam. Voluptate exercitation occaecat nostrud nostrud est elit ex magna do
+                et exercitation. Velit culpa anim velit sunt sunt eiusmod adipisicing qui cillum pariatur amet.
+                Reprehenderit nostrud consequat laborum nulla reprehenderit do incididunt tempor enim consectetur. Do do
+                non ea mollit officia labore et minim ex culpa quis.
+              </div>,
+              <div>
+                Dolore culpa duis fugiat id commodo exercitation irure occaecat culpa tempor ea. Cupidatat et enim
+                dolore aute amet. Duis labore esse Lorem ullamco elit sunt eu. Dolor do tempor veniam dolore nulla
+                laborum culpa ipsum mollit irure aliquip irure. Dolore aliquip dolor exercitation laborum sit. Magna
+                duis in voluptate consectetur duis. Minim irure reprehenderit non adipisicing laboris adipisicing enim
+                cillum magna veniam.
+              </div>,
+              <div>
+                Mollit Lorem quis amet quis sunt ad. Excepteur occaecat proident ad sunt enim minim. Eu duis eu magna
+                amet incididunt id nisi veniam ad nulla sit consequat.
+              </div>,
+            ][tab]
+          }
         </div>
       ) : (
         <div>없는 제품입니다</div>
