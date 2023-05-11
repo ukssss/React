68996d84c3acd219e6e855b9c567522943270af2
ukssss <1872003115su@gmail.com>
2023-05-12 00:21:16 +0900

    04-coding-apple-typescript | typescript 기본 타입 정리 (primitive types)

---
+++ 04.coding-apple-typescript/practice/index.ts
@@ -0,0 +1,24 @@
+let 이름: string = 'ukss';
+let 나이: number = 26;
+let 결혼유무: boolean = false;
+
+let 회원목록: string[] = ['ukss', 'hong'];
+let 회원목록2: { member1: string; member2: string } = { member1: 'ukss', member2: 'hong' };
+let 회원목록3 = 'ukss';
+let 회원목록4 = 26;
+
+// 과제
+// Q1
+const nickname = 'ukss';
+const age = 26;
+const hometown = 'masan';
+
+// Q2
+const playlist: { title: string; singer: string } = { title: '외딴섬 로맨틱', singer: 'CartheGarden' };
+
+// Q3
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+};
