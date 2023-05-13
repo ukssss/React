5d6e7033dae09436e3a20a5f641fb5edeefdab94
ukssss <1872003115su@gmail.com>
2023-05-13 16:22:06 +0900

    04-coding-apple-typescript | 타입 확정하기 Narrowing & Assertion

--- 04.coding-apple-typescript/practice/index.js
+++ 04.coding-apple-typescript/practice/index.js
@@ -1,36 +1,36 @@
-// hw 1
 function 함수(x) {
-    if (x) {
-        console.log("\uC548\uB155\uD558\uC138\uC694 ".concat(x));
+    var array = [];
+    array[0] = x;
+}
+var 이름 = 'ukss';
+함수(123);
+// hw1
+function hw1(x) {
+    var temp = [];
+    x.forEach(function (el) {
+        if (typeof el === 'string') {
+            temp.push(parseFloat(el));
+        }
+        else {
+            temp.push(el);
+        }
+    });
+    return temp;
+}
+console.log(hw1([123, '3']));
+// hw2
+var 철수쌤 = { subject: 'math' };
+var 영희쌤 = { subject: ['science', 'english'] };
+var 민수쌤 = { subject: ['science', 'art', 'korean'] };
+function hw2(x) {
+    if (typeof x.subject === 'string') {
+        return x.subject;
+    }
+    else if (Array.isArray(x.subject)) {
+        return x.subject[x.subject.length - 1];
     }
     else {
-        console.log('이름이 없습니다');
+        return 'Nothing,,';
     }
 }
-함수();
-// hw 2
-function 함수2(x) {
-    var arr = x.toString().split('');
-    return arr.length;
-}
-console.log(함수2('123'));
-// hw 2 선생님 답안
-function 함수3(x) {
-    return x.toString().length;
-}
-console.log(함수3(123));
-// hw 3
-function 결혼가능확률(월소득, 집보유여부, 매력점수) {
-    var cnt = 월소득;
-    if (집보유여부) {
-        cnt += 500;
-    }
-    if (매력점수 === '상') {
-        cnt += 100;
-    }
-    if (cnt >= 600) {
-        return '결혼가능';
-    }
-}
-console.log(결혼가능확률(700, false, '중'));
-console.log(결혼가능확률(100, false, '상'));
+console.log(hw2(철수쌤));
--- 04.coding-apple-typescript/practice/index.ts
+++ 04.coding-apple-typescript/practice/index.ts
@@ -6,3 +6,38 @@ function 함수(x: number | string) {
 let 이름: string = 'ukss';
 
 함수(123);
+
+// hw1
+
+function hw1(x: (number | string)[]) {
+  let temp: number[] = [];
+
+  x.forEach((el) => {
+    if (typeof el === 'string') {
+      temp.push(parseFloat(el));
+    } else {
+      temp.push(el);
+    }
+  });
+
+  return temp;
+}
+
+console.log(hw1([123, '3']));
+
+// hw2
+let 철수쌤 = { subject: 'math' };
+let 영희쌤 = { subject: ['science', 'english'] };
+let 민수쌤 = { subject: ['science', 'art', 'korean'] };
+
+function hw2(x: { subject: string | string[] }) {
+  if (typeof x.subject === 'string') {
+    return x.subject;
+  } else if (Array.isArray(x.subject)) {
+    return x.subject[x.subject.length - 1];
+  } else {
+    return 'Nothing,,';
+  }
+}
+
+console.log(hw2(철수쌤));
