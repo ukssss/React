d8d469ff5b3d54881ecad506427f3f17514938a9
ukssss <1872003115su@gmail.com>
2023-05-19 00:56:59 +0900

    04-coding-apple-typescript | never 타입

--- 04.coding-apple-typescript/practice/index.js
+++ 04.coding-apple-typescript/practice/index.js
@@ -1,10 +1,3 @@
-function 함수(x) {
-    if (x.wheel === '4개') {
-        console.log("\uC790\uB3D9\uCC28 \uBC14\uD034 ".concat(x.wheel));
-    }
-    else {
-        console.log("\uBC14\uC774\uD06C \uBC14\uD034 ".concat(x.wheel));
-    }
+function 함수() {
+    while (true) { }
 }
-var test = { wheel: '4개', color: 'blue' };
-함수(test);
--- 04.coding-apple-typescript/practice/index.ts
+++ 04.coding-apple-typescript/practice/index.ts
@@ -1,21 +1,3 @@
-type Car = {
-  wheel: '4개';
-  color: string;
-};
-
-type Bike = {
-  wheel: '2개';
-  color: string;
-};
-
-function 함수(x: Car | Bike) {
-  if (x.wheel === '4개') {
-    console.log(`자동차 바퀴 ${x.wheel}`);
-  } else {
-    console.log(`바이크 바퀴 ${x.wheel}`);
-  }
+function 함수(): never {
+  while (true) {}
 }
-
-let test: Car = { wheel: '4개', color: 'blue' };
-
-함수(test);
